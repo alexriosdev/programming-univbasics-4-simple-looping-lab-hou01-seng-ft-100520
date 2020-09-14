307f596128190dbc866a7656663976285841cdfc
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-14 22:39:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,21 @@
 def loop_message_five_times(message)
   count = 0
-  while
+  while count < 5 do
+    puts message
+    count += 1
+  end
 end
 
-def loop_message_n_times(string, integer)
-  
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end 
 end
 
 def output_array(array)
+  
   
 end
 
