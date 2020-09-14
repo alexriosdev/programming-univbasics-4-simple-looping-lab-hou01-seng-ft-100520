264881b77caae8eeacbaaf5ec2260f95432e425f
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-14 22:34:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+def loop_message_five_times
+  
+end
+
+def loop_message_five_times
+  
+end
+
+def loop_message_five_times
+  
+end
+
+def loop_message_five_times
+  
+end
+
+def loop_message_five_times
+  
+end
